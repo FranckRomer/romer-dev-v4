c94967dabfbd437109ac84e9798e71981736707b
FranckRomer <paco_metal95@hotmail.com>
2026-02-10 14:58:20 -0600

    config astro https

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,15 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['romer-dev.com']
+    },
+    preview: {
+      host: true,
+      allowedHosts: ['romer-dev.com', 'www.romer-dev.com'],
+      port: 3000
+    }
   },
   integrations: [react()],
   output: 'static'
